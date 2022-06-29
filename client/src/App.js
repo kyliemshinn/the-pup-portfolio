a5de4a4de93ead919ac224dcdcb14c5d195797f4
Kylie Shinn <kyliemshinn@gmail.com>
2022-06-29 12:32:07 -0700

    create initial styling for navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
 // import './App.css';
-// import React from 'react';
-// import Profile from './pages/Profile';
-// import Home from './pages/Home';
-// import NavBar from './components/NavBar'
-// import { Route, Routes } from "react-router-dom";
+import React from 'react';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// function App() {
-//   return (
-//     <div className="App">
-//     <NavBar />
-//     <Routes>
-//       <Route path="/" element={<Home />} />
-//       <Route path="profile" element={<Profile />} />
-//     </Routes>
-//     </div>
-//   );
-// }
+function App() {
+    // const client = new ApolloClient({
+    //     uri: '/graphql',
+    //     cache: new InMemoryCache(),
+    //   });
+  return (
+    <Router forceRefresh={true}>
+    <div className="App">
+    <NavBar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
+    </div>
+    </Router>
+  );
+}
 
-// export default App;
+export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../images/logo-placeholder-image.png";
+import "../styles/NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <img src={Logo} alt="Logo" height="150px" width="150px" />
+        <div className="container-fluid">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to="/" className="navLink" >Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/profile" className="navLink" >Profile</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="navLink" >About Me</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="navLink" >Meet The Parents</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
